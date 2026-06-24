08a3e681518f3c4dc4fe700115b6f2c7f7593dba
Md-Iqbal786 <mriqbalkhan104@gmail.com>
2026-06-25 03:03:09 +0530

    Use environment variable for MongoDB connection URI

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -7,7 +7,7 @@ const connectDB = async ()=>{
     mongoose.connection.on('connected' , ()=>{
         console.log("DataBase Connected ");
     })
-    await mongoose.connect("mongodb+srv://mriqbal104786_db_user:jIpGGRDO2X4nkxXq@cluster0.ka9tzkp.mongodb.net/");
+    await mongoose.connect(process.env.MONGO_URI ||"mongodb+srv://mriqbal104786_db_user:jIpGGRDO2X4nkxXq@cluster0.ka9tzkp.mongodb.net/");
 }
 
 export default connectDB;
